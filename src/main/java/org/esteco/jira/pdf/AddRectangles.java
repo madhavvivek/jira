3af0287cff8d4d1bc57571b2670f7edbb335d1da
madhavvivek <madhavvivek@gmail.com>
2017-04-23 16:19:26 +0530

    Added rectangles to pdf and image

---
+++ src/main/java/org/esteco/jira/pdf/AddRectangles.java
@@ -0,0 +1,92 @@
+package org.esteco.jira.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import java.awt.*;
+
+public class AddRectangles {
+
+    public void doIt(String message, String outfile) throws Exception {
+        PDDocument doc = null;
+        try {
+            doc = new PDDocument();
+
+            PDFont font = PDType1Font.HELVETICA;
+            PDPage page = new PDPage(PDRectangle.A4);
+            page.setRotation(90);
+            doc.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+
+            //Setting the non stroking color
+            contentStream.setNonStrokingColor(Color.ORANGE);
+
+            //Drawing a rectangle
+            // contentStream.addRect(200, 650, 10000, 10000);
+            // contentStream.addRect(200, 650, 100, 100);
+
+            // This is for landscape with one bug rectangle
+            // contentStream.addRect(25, 25, 545, 790);
+
+            /*
+            This is for landscape with four rectangles
+            // second quadrant
+            contentStream.addRect(25, 25, 272, 395);
+            // first quadrant
+            contentStream.addRect(25, 420, 272, 395);
+            // third quadrant
+            contentStream.addRect(297, 25, 272, 395);
+            // fourth quadrant
+            contentStream.addRect(297, 420, 272, 395);
+            */
+
+            // Four story cards
+            contentStream.addRect(25, 25, 272, 395);
+            // For icon
+            contentStream.addRect(30, 30, 75, 385);
+            contentStream.addRect(30, 30, 75, 75);
+            contentStream.addRect(105, 30, 187, 385);
+
+            contentStream.addRect(25, 420, 272, 395);
+            // For icon
+            contentStream.addRect(30, 425, 75, 385);
+            contentStream.addRect(30, 425, 75, 75);
+            contentStream.addRect(105, 425, 187, 385);
+
+            contentStream.addRect(297, 25, 272, 395);
+            // For icon
+            contentStream.addRect(302, 30, 75, 385);
+            contentStream.addRect(302, 30, 75, 75);
+            contentStream.addRect(105, 30, 187, 385);
+
+            contentStream.addRect(297, 420, 272, 395);
+            // For icon
+            contentStream.addRect(302, 425, 75, 385);
+            contentStream.addRect(302, 425, 75, 75);
+            contentStream.addRect(105, 425, 187, 385);
+
+            //Drawing a rectangle
+            contentStream.fillAndStroke();
+
+            //Closing the ContentStream object
+            contentStream.close();
+
+            //Saving the document
+            doc.save(outfile);
+        } finally {
+            if (doc != null) {
+                doc.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        AddRectangles app = new AddRectangles();
+        app.doIt("This is a message!", "d:/test-rectangle.pdf");
+    }
+}
---
+++ src/main/java/org/esteco/jira/pdf/Document.java
@@ -0,0 +1,75 @@
+package org.esteco.jira.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.util.Matrix;
+
+import java.io.IOException;
+
+public class Document {
+
+    public void doIt(String message, String outfile) throws IOException {
+        // the document
+        PDDocument doc = null;
+        try {
+            doc = new PDDocument();
+
+            PDFont font = PDType1Font.HELVETICA;
+            PDPage page = new PDPage(PDRectangle.A4);
+            page.setRotation(90);
+            doc.addPage(page);
+            PDRectangle pageSize = page.getMediaBox();
+            float pageWidth = pageSize.getWidth();
+            float fontSize = 12;
+            float stringWidth = font.getStringWidth(message) * fontSize / 1000f;
+            float startX = 100;
+            float startY = 100;
+            PDPageContentStream contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.OVERWRITE, false);
+            // add the rotation using the current transformation matrix
+            // including a translation of pageWidth to use the lower left corner as 0,0 reference
+            contentStream.transform(new Matrix(0, 1, -1, 0, pageWidth, 0));
+            contentStream.setFont(font, fontSize);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(startX, startY);
+            contentStream.showText(message);
+            contentStream.newLineAtOffset(0, 100);
+            contentStream.showText(message);
+            contentStream.newLineAtOffset(100, 100);
+            contentStream.showText(message);
+            contentStream.endText();
+
+            contentStream.moveTo(startX - 2, startY - 2);
+            contentStream.lineTo(startX - 2, startY + 200 + fontSize);
+            contentStream.stroke();
+
+            contentStream.moveTo(startX - 2, startY + 200 + fontSize);
+            contentStream.lineTo(startX + 100 + stringWidth + 2, startY + 200 + fontSize);
+            contentStream.stroke();
+
+            contentStream.moveTo(startX + 100 + stringWidth + 2, startY + 200 + fontSize);
+            contentStream.lineTo(startX + 100 + stringWidth + 2, startY - 2);
+            contentStream.stroke();
+
+            contentStream.moveTo(startX + 100 + stringWidth + 2, startY - 2);
+            contentStream.lineTo(startX - 2, startY - 2);
+            contentStream.stroke();
+
+            contentStream.close();
+
+            doc.save(outfile);
+        } finally {
+            if (doc != null) {
+                doc.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document app = new Document();
+        app.doIt("This is a message string", "d:/test.pdf");
+    }
+}
